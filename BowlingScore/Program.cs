0e4a42abadbf5665ca7c2d9577aafb7e12fd74f4
Thiago Bordignon <thiagobordignon@gmail.com>
2016-05-05 16:01:11 +0100

    initial state of juan's bowling kata

---
+++ BowlingScore/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int CalculateScore(string input)
+        {
+            int result = 0;
+            bool isLastRoundAStrike =  false;
+            foreach (char round in input)
+            {
+
+                if (round == 'X')
+                {
+                    if (isLastRoundAStrike)
+                    {
+                        result = result + 10;
+                    }
+                    
+                    result = result + 10;
+                    isLastRoundAStrike = true;
+                }
+                else
+                {
+                    int singleRound;
+                    if (Int32.TryParse(round.ToString(), out singleRound))
+                    {
+                        isLastRoundAStrike = false;
+                        result = result + singleRound;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ bowlingTest1/ScoreCalculation.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using BowlingScore;
+    
+
+namespace bowlingTest1
+
+{
+    
+    public class BowlingGame
+    {
+        [Theory]
+        [InlineData("X|X|X|X|X|X|X|X|X|X||XX", 300)]
+        //[InlineData("0-|X|X|X|X|X|X|X|X|X||XX", 280)]
+        void CalculateScore(string input, int score)
+        {
+            int result;
+
+            result = Program.CalculateScore(input);
+
+            Assert.Equal(score, result);
+        }
+    }
+}
